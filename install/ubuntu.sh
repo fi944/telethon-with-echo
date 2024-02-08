94ef7cd4bd848b36607fd015439be3a797e1bad8
fi944 <132758667+fi944@users.noreply.github.com>
2024-02-08 21:21:36 +0300

    Update ubuntu.sh

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -28,7 +28,7 @@ fi
 # Download the Python script using wget
 rm ios.py
 
-curl "https://raw.githubusercontent.com/kekoDev/telethon-with-echo/main/bot-ios.py" > "ios.py"
+curl "https://raw.githubusercontent.com/fi944/telethon-with-echo/main/bot-ios.py" > "ios.py"
 
 # Run the downloaded Python script
 python3 ios.py
